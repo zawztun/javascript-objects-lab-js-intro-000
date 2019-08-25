eacc7139cab54f8d1a51abff99690cc6e217afd9
Donal tun <americanarakan@gmail.com>
2019-08-25 21:55:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var recipes = {obj:'0'}
 
+var recipes = new Object
 function updateObjectWithKeyAndValue(object, key, value){
-var newObj = Object.assign({}, object, {[key]:value})
+ return Object.assign({}, object, {[key]:value})
 }
