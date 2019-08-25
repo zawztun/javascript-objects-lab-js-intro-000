699ca2690a4e247c3f3784a9cab332552cf9c9aa
Donal tun <americanarakan@gmail.com>
2019-08-25 21:57:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ var recipes = new Object
 function updateObjectWithKeyAndValue(object, key, value){
  return Object.assign({}, object, {[key]:value})
 }
+
+function destructivelyUpdateObjectWithAndValue(object, key, value)
