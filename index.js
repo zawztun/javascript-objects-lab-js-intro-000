da8a686896727b8fd38be7726c66891e5ed08687
Donal tun <americanarakan@gmail.com>
2019-08-25 22:01:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object = {Key:value}
+  object = {[Key]:value}
   return object
 }
